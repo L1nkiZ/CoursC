2829ea98ea11b9a925d42cf10c65b7b48fa9b6ac
L1nkiZ <guillaume.gerard2112@outlook.fr>
2022-05-12 16:57:20 +0200

    Oui

--- TD6/ex3.c
+++ TD6/ex3.c
@@ -1,16 +1,46 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+
 
 typedef struct
-
 {
     int taille;
     int* contenu;
 }Tableau;
 
-int main(int argc, char const argv[])
+
+void* nombrespif(void* argument){
+    Tableau* tab = (Tableau)argument;
+    for (int i = 0; i < tab->taille; i++)
+    {
+        tab->contenu[i]=rand()%5+1;
+        sleep(1);
+    }
+    pthread_exit(NULL);
+
+}
+
+void affichage(void* argument){
+    Tableau* tab = (Tableau)argument;
+    for (int j = 0; j < tab->taille/2+1; j++)
+    {
+        for (int i = 0; i < tab->taille; i++)
+        {
+            printf("%d\n",tab->contenu[i]);
+        };
+        sleep(2);
+    }
+    pthread_exit(NULL);
+}
+
+int main(int argc, char constargv[])
 {
+
+    pthread_t monThread;
+    pthread_t monThread2;
     Tableau tab;
     int tempo;
 
@@ -18,7 +48,7 @@ int main(int argc, char const argv[])
     scanf("%d", &tempo);
 
     tab.taille=tempo;
-    tab.contenu=(int)malloc(tempo*sizeof(int));
+    tab.contenu=(int)malloc(temposizeof(int));
 
     for (int i = 0; i < tempo; i++)
     {
@@ -26,7 +56,16 @@ int main(int argc, char const argv[])
     };
 
 
+    pthread_create(&monThread, NULL, nombrespif, (void) &tab);
+    pthread_create(&monThread2, NULL, affichage, (void) &tab);
+
+    pthread_join(monThread, NULL);
+    pthread_join(monThread2, NULL);
+/*
     for (int i = 0; i < tempo; i++)
     {
-        printf("%d", tab.contenu[i]);
+        printf("%d\n",tab.contenu[i]);
     };
+*/
+    return 0;
+}
